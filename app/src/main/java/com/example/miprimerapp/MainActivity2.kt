9f967ada1ac83efba7fb8e0e08dc1f6dcfe5bf2b
Camilo Tacue Salazar <cetacue@misena.edu.co>
2023-10-25 01:12:56 -0500

    .__.

--- app/src/main/java/com/example/miprimerapp/MainActivity2.kt
+++ app/src/main/java/com/example/miprimerapp/MainActivity2.kt
@@ -1,11 +1,23 @@
 package com.example.miprimerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var boton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        boton = findViewById(R.id.botonSalir)
+        boton.setOnClickListener{
+            val intentdos = Intent(this, MainActivity::class.java)
+            startActivity(intentdos)
+
+            finish()
+        }
     }
 }
